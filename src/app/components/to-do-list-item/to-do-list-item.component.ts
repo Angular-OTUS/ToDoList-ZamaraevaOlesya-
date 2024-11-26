50d488a2d960a4bfcfae0f1bb5ca073e24c9da69
olesi <olesia.zamar@gmail.com>
2024-11-27 01:43:14 +0700

    homework-2 - вынос списка задач, добавление и удаление задач, стилизация

---
+++ src/app/components/to-do-list-item/to-do-list-item.component.ts
@@ -0,0 +1,19 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {TodoListInterface} from "../../interfaces/todo-list.interface";
+
+@Component({
+  selector: 'app-to-do-list-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './to-do-list-item.component.html',
+  styleUrls: ['./to-do-list-item.component.scss']
+})
+export class ToDoListItemComponent {
+@Input() todoList: Array<TodoListInterface> = []
+@Output('removeTask') removeTask = new EventEmitter<any>();
+
+public removeTasks(id: number) {
+  this.removeTask.emit(id)
+}
+}
